ccb09fd13707c197f0962db013880fda9b221a2c
MatChumski <mateo0704@hotmail.com>
2022-12-01 01:23:04 -0500

    Configuración para Azure

--- Models/GastonDbContext.cs
+++ Models/GastonDbContext.cs
@@ -23,7 +23,8 @@ public partial class GastonDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-6QTKH2L\\SQLEXPRESS;Database=GastonDB; Trusted_Connection=True; TrustServerCertificate=True");
+        //=> optionsBuilder.UseSqlServer("Server=DESKTOP-6QTKH2L\\SQLEXPRESS;Database=GastonDB; Trusted_Connection=True; TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Server=gastonapidbserver.database.windows.net;Initial Catalog=GastonDB;Persist Security Info=False;User ID=matchumski;Password=02040521Lol*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
